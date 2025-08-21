ef02d1df78ff03ad0ef126a2addff9045ed1a457
Mark Richardson <mrichardson82@gmail.com>
2025-08-21 10:20:24 +0100

    Convert ipynb to marimo

---
+++ book/marimo/demo.py
@@ -0,0 +1,31 @@
+import marimo
+
+__generated_with = "0.10.9"
+app = marimo.App()
+
+
+@app.cell
+def __import_libs():
+    import dummypy as dp
+
+    return (dp,)
+
+
+@app.cell
+def __create_grid(dp):
+    grid = dp.Grid()
+    return (grid,)
+
+
+@app.cell
+def __show_grid(grid):
+    grid
+
+
+@app.cell
+def __show_diff(grid):
+    grid.diff()
+
+
+if __name__ == "__main__":
+    app.run()
