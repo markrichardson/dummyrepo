648d976ba5e107fd49c31e65f0c258db82b283b2
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2026-02-07 09:00:42 +0400

    sync

--- .rhiza/make.d/01-test.mk
+++ .rhiza/make.d/01-test.mk
@@ -23,25 +23,21 @@ COVERAGE_FAIL_UNDER ?= 90
 test: install ## run all tests
 	@rm -rf _tests;
 
-	@if [ -d ${TESTS_FOLDER} ]; then \
-	  mkdir -p _tests/html-coverage _tests/html-report; \
-	  if [ -d ${SOURCE_FOLDER} ]; then \
-	    ${VENV}/bin/python -m pytest ${TESTS_FOLDER} \
-	    --ignore=${TESTS_FOLDER}/benchmarks \
-	    --cov=${SOURCE_FOLDER} \
-	    --cov-report=term \
-	    --cov-report=html:_tests/html-coverage \
-	    --cov-fail-under=$(COVERAGE_FAIL_UNDER) \
-	    --cov-report=json:_tests/coverage.json \
-	    --html=_tests/html-report/report.html; \
-	  else \
-	    printf "${YELLOW}[WARN] Source folder ${SOURCE_FOLDER} not found, running tests without coverage${RESET}\n"; \
-	    ${VENV}/bin/python -m pytest ${TESTS_FOLDER} \
-	    --ignore=${TESTS_FOLDER}/benchmarks \
-	    --html=_tests/html-report/report.html; \
-	  fi \
+	@mkdir -p _tests/html-coverage _tests/html-report; \
+	if [ -d ${SOURCE_FOLDER} ]; then \
+	  ${VENV}/bin/python -m pytest \
+	  --ignore=${TESTS_FOLDER}/benchmarks \
+	  --cov=${SOURCE_FOLDER} \
+	  --cov-report=term \
+	  --cov-report=html:_tests/html-coverage \
+	  --cov-fail-under=$(COVERAGE_FAIL_UNDER) \
+	  --cov-report=json:_tests/coverage.json \
+	  --html=_tests/html-report/report.html; \
 	else \
-	  printf "${YELLOW}[WARN] Test folder ${TESTS_FOLDER} not found, skipping tests${RESET}\n"; \
+	  printf "${YELLOW}[WARN] Source folder ${SOURCE_FOLDER} not found, running tests without coverage${RESET}\n"; \
+	  ${VENV}/bin/python -m pytest \
+	  --ignore=${TESTS_FOLDER}/benchmarks \
+	  --html=_tests/html-report/report.html; \
 	fi
 
 # The 'typecheck' target runs static type analysis using mypy.
